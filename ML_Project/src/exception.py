78e2f8165302f1965f155e573ed52166f9af855f
unknown <rudrashishpalav@gmail.com>
2026-01-14 22:50:28 +0100

    Correction to Logger and Custom Exception

--- ML_Project/src/exception.py
+++ ML_Project/src/exception.py
@@ -1,4 +1,5 @@
 import sys
+import logging
 
 def error_message_detail(error, error_detail:sys):
     _,_,exc_tb = error_detail.exc_info() # to get exception info from the third part of exc_info
@@ -16,3 +17,11 @@ class CustomException(Exception):
 
     def __str__(self):
         return self.error_message
+    
+if __name__ == "__main__":
+    try:
+        a = 1 / 0
+    except Exception as e:
+        logging.info("Divide by zero")
+        raise CustomException(e, sys)
+        
